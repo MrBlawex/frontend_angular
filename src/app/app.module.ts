2a3fefc9ca28eb9461a3ca0b4e576ff634116370
Eduard <edikbelousov@gmail.com>
2020-02-04 13:25:05 +0200

    Create Clean Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductsAddComponent } from './components/products-add/products-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    ProductsListComponent,
+    ProductsAddComponent
   ],
   imports: [
     BrowserModule,
