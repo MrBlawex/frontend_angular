bb86fb1318d84bb5a897c9b79093a03beebec5f0
BeerBaron322 <55479964+BeerBaron322@users.noreply.github.com>
2020-02-12 00:47:58 +0200

    Code refactoring

--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -4,22 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  // Вывод токена из LocalStroreg
 
   public getToken() {
     return localStorage.getItem('token');
   }
 
-  // Запись токена в LocalStroreg
   public setToken(token: string) {
     localStorage.setItem('token', token);
-  }
-
-  public isHasToken() {
-    if (this.getToken() === null) {
-      return false;
-    }
-    return true;
   }
 
   public deleteToken() {
